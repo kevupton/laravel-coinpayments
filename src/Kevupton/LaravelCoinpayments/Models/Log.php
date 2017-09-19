64e86f30262d11466a9a3dffa740c6ba332f9596
Kevin Upton <k_upton@live.com>
2017-09-19 19:53:11 +1000

    updated the log

--- src/Kevupton/LaravelCoinpayments/Models/Log.php
+++ src/Kevupton/LaravelCoinpayments/Models/Log.php
@@ -10,11 +10,13 @@ namespace Kevupton\LaravelCoinpayments\Models;
 
 class Log extends Model
 {
+    protected $table = 'log';
+
     const LEVEL_ALL = 2;
     const LEVEL_ERROR = 1;
     const LEVEL_NONE = 0;
 
     public $fillable = [
-        ''
+        'type', 'log'
     ];
 }
