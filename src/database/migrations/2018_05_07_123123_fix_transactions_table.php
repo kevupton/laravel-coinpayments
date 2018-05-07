38d1ca677f2f731212c493456ef24c007451356a
Kevin Upton <k_upton@live.com>
2018-05-07 11:43:08 +1000

    renamed migration

--- src/database/migrations/2018_05_07_123123_fix_transactions_table.php
+++ src/database/migrations/2018_05_07_123123_fix_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCallbackAddressTable extends Migration
+class FixTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
