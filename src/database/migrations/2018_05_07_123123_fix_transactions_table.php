9a8396cc67ff500d9c14d4de479b7512339c43cd
Kevin Upton <k_upton@live.com>
2018-05-07 11:32:14 +1000

    migration fix

---
+++ src/database/migrations/2018_05_07_123123_fix_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCallbackAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $prefix = cp_table_prefix();
+
+        Schema::table($prefix . 'transactions', function (Blueprint $table) {
+            $table->renameColumn('amount', 'amount2');
+            $table->string('amount1')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $prefix = cp_table_prefix();
+
+        Schema::table($prefix . 'transactions', function (Blueprint $table) {
+            $table->dropColumn('amount1');
+            $table->renameColumn('amount2', 'amount');
+        });
+    }
+}
